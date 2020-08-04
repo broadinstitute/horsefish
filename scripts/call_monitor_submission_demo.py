4b4085aa09099d17b6a72768376ef05363efd907
Marianie-Simeon <msimeon@broadinstitue.org>
2020-08-04 17:34:03 -0400

    Update call_monitor_submission_demo.py

--- scripts/call_monitor_submission_demo.py
+++ scripts/call_monitor_submission_demo.py
@@ -2,33 +2,33 @@
 from firecloud import api as fapi
 from firecloud import errors as ferrors
 
+# Adjustable Args*
 workspace_name = 'HCA_Optimus_Pipeline_MS'
 workspace_project = 'broad-firecloud-dsde'
 submission_id = '990e5930-a9b8-4f70-b220-11b7ffe702bb'
-# workflow_name has to be unique everytime
-workflow_name = 'monitor_submission_22'
+workflow_repo = 'horsefish'
+workflow_name = 'monitor_submission'
 
-json_body = {
-    "namespace": 'horsefish',
-    "name": workflow_name,
-    'methodConfigVersion': 14,
-    'methodRepoMethod': {'methodName': 'monitor_submission', 'methodVersion': 14, 'methodNamespace': 'horsefish', 'methodUri': 'agora://horsefish/monitor_submission/14', 'sourceRepo': 'agora'},
-    "inputs": {"monitor_submission.submission_id": f'"{submission_id}"', "monitor_submission.terra_project": f'"{workspace_project}"', "monitor_submission.terra_workspace": f'"{workspace_name}"'},
-    "outputs": {},
-    "prerequisites": {},
-    'deleted': False,
-    'source': 'agora://horsefish/monitor_submission/14'
-}
-create_workflow_response = fapi.create_workspace_config(workspace_project, workspace_name, json_body)
-if create_workflow_response.status_code != 201:
-    print(create_workflow_response.content)
-    raise ferrors.FireCloudServerError(create_workflow_response.status_code, create_workflow_response.content)
+# Getting the json for the Monitor Submission Workflow
+workflow = fapi.get_workspace_config(workspace_project, workspace_name, workflow_repo, workflow_name)
+if workflow.status_code != 200:
+    print(workflow.content)
+    raise ferrors.FireCloudServerError(workflow.status_code, workflow.content)
 else:
-    print("Successfully Created Workflow")
-    create_submisson_response = fapi.create_submission(workspace_project, workspace_name, 'horsefish', workflow_name, entity=None, etype=None, expression=None, use_callcache=True)
-    if create_submisson_response.status_code != 201:
-        print(create_submisson_response.content)
-        raise ferrors.FireCloudServerError(create_submisson_response.status_code, create_submisson_response.content)
+    workflow_json = workflow.json()
+
+    # Updating the inputs in the JSON
+    workflow_json['inputs'] = {"monitor_submission.submission_id": f'"{submission_id}"', "monitor_submission.terra_project": f'"{workspace_project}"', "monitor_submission.terra_workspace": f'"{workspace_name}"'}
+    updated_workflow = fapi.update_workspace_config(workspace_project, workspace_name, workflow_repo, workflow_name, workflow_json)
+    if updated_workflow.status_code != 200:
+        print(updated_workflow.content)
+        raise ferrors.FireCloudServerError(updated_workflow.status_code, updated_workflow.content)
     else:
-        print("Successfully Created Submisson")
-        print(create_submisson_response.json())
+        # Launching the Updated Monitor Submission Workflow
+        create_submisson_response = fapi.create_submission(workspace_project, workspace_name, workflow_repo, workflow_name, entity=None, etype=None, expression=None, use_callcache=True)
+        if create_submisson_response.status_code != 201:
+            print(create_submisson_response.content)
+            raise ferrors.FireCloudServerError(create_submisson_response.status_code, create_submisson_response.content)
+        else:
+            print("Successfully Created Submisson")
+            print(create_submisson_response.json())
