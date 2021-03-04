523257a1715918f4b84b565151ae509e65349837
Sushma Chaluvadi <schaluva@broadinstitute.org>
2021-03-04 11:16:57 -0800

    update with column types from input file

--- scripts/anvil_tools/post_workspace_attributes.py
+++ scripts/anvil_tools/post_workspace_attributes.py
@@ -17,12 +17,11 @@ def setup_single_data_delivery_workspace(request, workspace_name, project="anvil
     workspace_dict = {"input_workspace_name": "NA",
                       "input_project_name": "NA",
                       "workspace_link": "NA",
-                      "dataset_attributes_status": "NA",
+                      "post_dataset_attributes_status": "NA",
                       "publish_workspace_status": "Incomplete",
                       "final_workspace_status": "Failed"}
 
-    # update the workspace's dataset attributes per request in list of requests
-    # workspace_name = request["name"]
+    # set basic information about workspace to dictionary (to output in csv file)
     workspace_dict["input_workspace_name"] = workspace_name
     workspace_dict["input_project_name"] = project
     workspace_dict["workspace_link"] = (f"https://app.terra.bio/#workspaces/{project}/{workspace_name}").replace(" ", "%20")
@@ -31,7 +30,7 @@ def setup_single_data_delivery_workspace(request, workspace_name, project="anvil
     post_attrs_success, post_attrs_response = add_library_metadata_to_workspace(request, workspace_name, project)
 
     if not post_attrs_success:  # if posting dataset attributes does not succeed
-        workspace_dict["dataset_attributes_status"] = post_attrs_response
+        workspace_dict["post_dataset_attributes_status"] = post_attrs_response
         return workspace_dict
 
     workspace_dict["post_dataset_attributes_status"] = "Success"
@@ -44,35 +43,60 @@ def setup_single_data_delivery_workspace(request, workspace_name, project="anvil
         return workspace_dict
 
     workspace_dict["publish_workspace_status"] = "Success"
+    workspace_dict["final_workspace_status"] = "Success"
     return workspace_dict
+
+
+def format_dataframe(workspace_row):
+    """Format workspace row Series object and convert to json."""
+
+    list_dtype_cols = ["library:consentCodes",
+                       "library:dataCategory",
+                       "library:dataFileFormats",
+                       "library:datatype",
+                       "library:DS"
+                       "library:DS_URL",
+                       "library:ethnicity",
+                       "library:institute",
+                       "library:profilingProtocol",
+                       "library:technology",
+                       "tag:tags"]
+
+    for key, value in workspace_row.items():
+        if key in list_dtype_cols:
+            if isinstance(value, str):
+                new_value = value.strip('[]')
+                workspace_row[key] = [new_value]
+
+    return workspace_row.to_json()
 
 
 def setup_data_delivery_workspaces(tsv, project="anvil-datastorage"):
     """Post dataset attributes to workspace and publish workspace to Data Library (FireCloud)."""
 
     # read input tsv into dataframe, workspace name = index and edit dataframe
-    all_workspaces = pd.read_csv(tsv, sep="\t", index_col="name")
-    # remove columns that have ".itemsType" in col name - col values are AttributeValue
-    all_workspaces_modified = all_workspaces[all_workspaces.columns.drop(list(all_workspaces.filter(regex='.itemsType')))]
+    all_workspaces_df = pd.read_csv(tsv, sep=",", index_col="name", keep_default_na=False)
+    # remove cols w ".itemsType" in col name - values are "AttributeValue"
+    all_workspaces_modified_df = all_workspaces_df[all_workspaces_df.columns.drop(list(all_workspaces_df.filter(regex='.itemsType')))]
     # remove the ".items" string from column names
-    all_workspaces_modified.columns = all_workspaces_modified.columns.str.replace(".items", "")
+    all_workspaces_modified_df.columns = all_workspaces_modified_df.columns.str.replace(".items", "")
 
     # create df for OUTPUT tsv file
     col_names = ["input_workspace_name", "input_project_name",
                  "workspace_link",
-                 "post_dataset_attributes_status", "publish_workspace_error",
+                 "post_dataset_attributes_status", "publish_workspace_status",
                  "final_workspace_status"]
+
     published_workspace_statuses = pd.DataFrame(columns=col_names)
 
     # make json request for each workspace and append json request to list of requests
-    # row_requests = []
-    # for index, row in all_workspaces_modified.iterrows():
-    for workspace in all_workspaces_modified.index:
-        row_json_request = all_workspaces_modified.loc[workspace].to_json()
-        # row_requests.append(row_json_request)
+    for workspace in all_workspaces_modified_df.index:
+        row_json_request = all_workspaces_modified_df.loc[workspace]
 
-    # for request in row_requests:
-        row_dict = setup_single_data_delivery_workspace(row_json_request, workspace, project)
+        # create validated json
+        validated_json = format_dataframe(row_json_request)
+
+        row_dict = setup_single_data_delivery_workspace(validated_json, workspace, project)
 
         # append single workspace's dictionary of statuses to output dataframe
         published_workspace_statuses = published_workspace_statuses.append(row_dict, ignore_index=True)
