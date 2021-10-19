24dcbdf8952022bb13d1365d8665e1eb25b58e32
Sushma Chaluvadi <schaluva@broadinstitute.org>
2021-10-19 14:48:55 -0400

    finish structure and validations

--- scripts/cfos/make_dataset_data_tables.py
+++ scripts/cfos/make_dataset_data_tables.py
@@ -3,51 +3,77 @@
 Usage:
     > python3 make_dataset_data_tables.py -d DATASET_TYPE -x EXCEL_FILE -w TERRA_WORKSPACE_NAME -p TERRA_WORKSPACE_PROJECT """
 import argparse
+from firecloud import api as fapi
 import json
 import pandas as pd
 from pandas_schema import*
 from pandas_schema.validation import*
 from validate import DATA_TABLE_VALIDATE_AND_FORMAT_SCHEMA as schema_validator
 
-def validate_and_format_dataset_tables(table_metadata, table_name):
-    """Validate and/or format a dataset's data tables with defined validations/formats."""
 
-    # validate the data frame for given table
-    # validated_table = validate_and_format_table(table_df, table)
-    # write validated data table to tsv file in data table upload format
-    print(table_name)
-    errors = validator.validate(table_metadata)
-    print(errors)
-    errors_index_rows = [e.row for e in errors]
-    print(errors_index_rows)
-    data_clean = table_metadata.drop(index=errors_index_rows)
-    print(data_clean)
+def upload_dataset_table_to_workspace(dataset_tables_dict, workspace_name, workspace_project):
+    """Upload each of the tables in the chosen dataset to given workspace."""
+    
+    # for each table in dictionary
+    for table in dataset_tables_dict:
+        table_df = dataset_tables_dict[table]
+        output_filename = f"{table}_table_load_file.tsv"
+        # write out tsv file per table
+        table_df.to_csv(output_filename, sep="\t")
 
-    pd.DataFrame({'col':errors}).to_csv('errors.csv')
-    data_clean.to_csv('clean_data.csv')
-    exit(1)
+        response = fapi.upload_entities_tsv(workspace_project, workspace_name, output_filename, model='flexible')
+        status_code = response.status_code
 
-def create_dataset_tables_dictionary(dataset_metadata, dataset_name, dataset_table_names, schema_dict):
+        # TODO: do we want it to fail entirely as long as a single table fails to upload
+        # if not success
+        if status_code != 200:
+            print(f"Warning: Table with name {table} has failed to upload. Please see errors: {response.text}")
+            return
+
+        # if success
+        print(f"Table with name {table} has uploaded to {workspace_project}/{workspace_name}.")
+
+
+def create_dataset_tables_dictionary(validated_dataset, dataset_name, dataset_table_names, schema_dict):
     """For a given dataset, subset/separate input columns into individual table dataframes."""
 
     # instantiate empty dictionary to capture {table_name: table_metadata_dataframe}
     dataset_tables_dict = {}
 
-    # create a subset df for each table defined for given dataset and send for pertinent validation
+    # create a subset df for each table defined in the chosen dataset
     for table in dataset_table_names:
         # get subset dataframe for specific table for given dataset name
         table_df = dataset_metadata[schema_dict[dataset_name][table]]
+
+        # TODO: edit the value = dataframe for a table to have the entity:table_name_id format
+
         # add KVP {table name : table_df} entry to all tables dictionary
         dataset_tables_dict[table] = table_df
 
-    # dictionary of table and table dataframes
-    # print(dataset_tables_dict)
-    for key in dataset_tables_dict:
-        table_name = key
-        table_df = dataset_tables_dict[key]
-        validate_and_format_dataset_tables(table_df, table_name)
-    exit(1)
-        # validate_and_format_dataset_tables(dataset, table_name)
+    print(f"Dataset has been partitioned into the following individual tables: {dataset_table_names}")
+    return dataset_tables_dict
+
+
+def validate_dataset(dataset):
+    """Validate the full dataset metadata with Schema validator."""
+
+    # capture errors from the validation against the pre-defined validations per column
+    errors = schema_validator.validate(dataset, columns=schema_validator.get_column_names())
+    # get indices of the failed rows
+    errors_index_rows = [e.row for e in errors]
+
+    # if any errors, print error message and exit
+    if errors_index_rows:
+        print(f"Warning: Dataset validation errors found. Please retry after correcting errors listed in validation_errors.csv.")
+        pd.DataFrame({'validation_error':errors}, index=False).to_csv('validation_errors.csv')
+        return
+
+    # TODO: determine how to handle drops of failed rows/columns to get cleaned data
+    # errors_index_rows = [e.row for e in errors]
+    # data_clean = dataset_metadata_df.drop(index=errors_index_rows, axis=1)
+    # data_clean.to_csv('clean_data.csv')
+    print(f"Dataset has been validated.")
+    return dataset
 
 
 def organize_dataset_metadata(dataset_name, excel, schema_json):
@@ -80,19 +106,6 @@ def organize_dataset_metadata(dataset_name, excel, schema_json):
         return
 
     print(f"{len(dataset_table_names)} tables will be created for {dataset_name}: {dataset_table_names}")
-    # print(f"Dataset metadata dataframe: {dataset_metadata_df}")
-
-    errors = schema_validator.validate(dataset_metadata_df, columns=schema_validator.get_column_names())
-    print(errors)
-    errors_index_rows = [e.row for e in errors]
-    print(errors_index_rows)
-    exit(1)
-    data_clean = table_metadata.drop(index=errors_index_rows)
-    print(data_clean)
-
-    pd.DataFrame({'col':errors}).to_csv('errors.csv')
-    data_clean.to_csv('clean_data.csv')
-    exit(1)
     return(dataset_metadata_df, dataset_table_names, schema_dict)
 
 
@@ -110,7 +123,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     dataset_metadata, dataset_table_names, schema_dict = organize_dataset_metadata(args.dataset, args.excel, args.schema)
-    dataset_tables_dict = create_dataset_tables_dictionary(dataset_metadata, args.dataset, dataset_table_names, schema_dict)
-    validate_and_format_dataset_tables(table_metadata, table_name)
-
+    validated_dataset = validate_dataset(dataset_metadata)
+    dataset_tables_dict = create_dataset_tables_dictionary(validated_dataset, args.dataset, dataset_table_names, schema_dict)
+    upload_dataset_table_to_workspace(dataset_tables_dict, args.workspace, args.project)
 # python3 make_dataset_data_tables.py -d dataset1 -x test_data/CFoS_Template.xlsx -j dataset_tables_schema.json -p project -w workspace
--- scripts/cfos/validate.py
+++ scripts/cfos/validate.py
@@ -1,60 +1,69 @@
 """Validate dataframes with validators."""
 from pandas_schema import*
 from pandas_schema.validation import*
+import re
 
-
+# validators
 # column values cannot be null
 null_validation = [CustomElementValidation(lambda d: d is not np.nan, 'this field cannot be null')]
 
+# validator options
+has_organism_type_options = ["C elegans", "Canis lupus familiaris (dog)", "Ciona intestinalis (Ascidian)",
+                             "Danio rerio (zebrafish)", "Drosophila melanogaster (fruit fly)", 
+                             "Homo sapiens (human)", "Mus musculus (mouse)", "Oryctolagus cuniculus (rabbit)",
+                             "Paracentrotus lividus (Rock sea urchin)", "Rattus norvegicus (rat)",
+                             "Sphaerechinus granularis (Violet sea urchin)"]
+has_phenotypic_sex_options = ["Male", "Female", "Intersex"]
+neuropathology_options = ["Abeta", "AbetaTau", "None reported"]
+has_biosample_type_options = ["CellLine", "Derived_In vitro differentiated", "Derived_Induced pluripotent stem cell",
+                              "Derived_Organoid", "BodyFluid_Amniotic fluid", "Blood_Buffy coat", "Blood_Erythrocyte",
+                              "Blood_Leukocyte", "Blood_PBMC", "Blood_Plasma", "Blood_Platelet", "Blood_Serum",
+                              "Blood_Whole Blood", "BodyFluid_BreastMilk", "BodyFluid_Cerebrospinal fluid",
+                              "BodyFluid_Saliva", "BodyFluid_Semen", "BodyFluid_Synovial fluid", "BodyFluid_Urine",
+                              "BodyFluid_Vaginal secretions", "Cell free DNA", "Cell_BCell", "Cell_Lymphocyte",
+                              "Cell_Monocyte", "Cell_T cell", "Primary Culture", "Stool", "Tissue"]
+data_modality_options = ["Epigenomic", "Epigenomic_3D Contact Maps", "Epigenomic_DNABinding",
+                         "Epigenomic_DNABinding_HistoneModificationLocation", "Epigenomic_DNABinding_TranscriptionFactorLocation",
+                         "Epigenomic_DNAChromatinAccessibility", "Epigenomic_DNAMethylation", "Epigenomic_RNABinding", "Genomic",
+                         "Genomic_Assembly", "Genomic_Exome", "Genomic_Genotyping_Targeted", "Genomic_WholeGenome",
+                         "Imaging_Electrophysiology", "Imaging_Microscopy", "Medical imaging _CTScan", "Medical imaging _Electrocardiogram",
+                         "Medical imaging _MRI", "Medical imaging _Xray", "Metabolomic", "Microbiome", "Metagenomic", "Proteomic",
+                         "Transcriptomic", "SpatialTranscriptomics", "Trascriptomic_Targeted", "Trascriptomic_NonTargeted"]
+has_disease_options = ["y", "n"]
+
+# schema validator definition
+# TODO: set required columns
 # validation schema by specific column name across any and all possible datasets - not specific to just a single dataset
-DATA_TABLE_VALIDATE_AND_FORMAT_SCHEMA = Schema([Column("donor_id", [null_validation]),
-                                                   Column("hasDonorAge", [IsDtypeValidation(int)]),
-                                                   Column("has_phenotypic_sex", [InListValidation(["Male", "Female", "Intersex"])]),
-                                                   Column("age_at_biopsy", [IsDtypeValidation(int)]),
-                                                   Column("hasOrganismType", [InListValidation(["C elegans", "Canis lupus familiaris (dog)",
-                                                                                                "Ciona intestinalis (Ascidian)", "Danio rerio (zebrafish)",
-                                                                                                "Drosophila melanogaster (fruit fly)", "Homo sapiens (human)",
-                                                                                                "Mus musculus (mouse)", "Oryctolagus cuniculus (rabbit)",
-                                                                                                "Paracentrotus lividus (Rock sea urchin)", "Rattus norvegicus (rat)",
-                                                                                                "Sphaerechinus granularis (Violet sea urchin)"])]),
-                                                   # TODO: are there a list of sites, or is this a free form field
-                                                   Column("hasAnatomicalSite", [None]),
-                                                   Column("neuropathology", [InListValidation(["Abeta", "AbetaTau", "None reported"])]),
-                                                   Column("biosample_id", [None]),
-                                                   Column("hasBioSampleType", [InListValidation(["CellLine", "Derived_In vitro differentiated", "Derived_Induced pluripotent stem cell",
-                                                                                                 "Derived_Organoid", "BodyFluid_Amniotic fluid", "Blood_Buffy coat", "Blood_Erythrocyte",
-                                                                                                 "Blood_Leukocyte", "Blood_PBMC", "Blood_Plasma", "Blood_Platelet", "Blood_Serum",
-                                                                                                 "Blood_Whole Blood", "BodyFluid_BreastMilk", "BodyFluid_Cerebrospinal fluid",
-                                                                                                 "BodyFluid_Saliva", "BodyFluid_Semen", "BodyFluid_Synovial fluid", "BodyFluid_Urine",
-                                                                                                 "BodyFluid_Vaginal secretions", "Cell free DNA", "Cell_BCell", "Cell_Lymphocyte",
-                                                                                                 "Cell_Monocyte", "Cell_T cell", "Primary Culture", "Stool", "Tissue"])]),
-                                                   Column("MMSE_Biopsy_value", [IsDtypeValidation(int)]),
-                                                   Column("MMSE_Biopsy_atDonorAge", [IsDtypeValidation(int)]),
-                                                   Column("MMSE_final_value", [IsDtypeValidation(int)]),
-                                                   Column("MMSE_final_atDonorAge", [IsDtypeValidation(int)]),
-                                                   Column("APOE_value", [IsDtypeValidation(int)]),
-                                                   Column("APOE_atDonorAge", [IsDtypeValidation(int)]),
-                                                   Column("DataModality", [InListValidation(["Epigenomic", "Epigenomic_3D Contact Maps", "Epigenomic_DNABinding",
-                                                                                             "Epigenomic_DNABinding_HistoneModificationLocation", "Epigenomic_DNABinding_TranscriptionFactorLocation",
-                                                                                             "Epigenomic_DNAChromatinAccessibility", "Epigenomic_DNAMethylation",
-                                                                                             "Epigenomic_RNABinding", "Genomic", "Genomic_Assembly", "Genomic_Exome",
-                                                                                             "Genomic_Genotyping_Targeted", "Genomic_WholeGenome", "Imaging_Electrophysiology",
-                                                                                             "Imaging_Microscopy", "Medical imaging _CTScan", "Medical imaging _Electrocardiogram",
-                                                                                             "Medical imaging _MRI", "Medical imaging _Xray", "Metabolomic", "Microbiome",
-                                                                                             "Metagenomic", "Proteomic", "Transcriptomic", "SpatialTranscriptomics",
-                                                                                             "Trascriptomic_Targeted", "Trascriptomic_NonTargeted"])]),
-                                                   # file path columns must start with "gs://"
-                                                   Column("summary_file_path", [MatchesPatternValidation(r"^gs://*$")]),
-                                                   Column("features_file_path", [MatchesPatternValidation(r"^gs://*$")]),
-                                                   Column("matrix_file_path", [MatchesPatternValidation(r"^gs://*$")]),
-                                                   Column("barcode_file_path", [MatchesPatternValidation(r"^gs://*$")]),
-                                                   Column("biosample_id_of_file", [MatchesPatternValidation(r"^gs://*$")]),
-                                                   Column("library_id", [None]),
-                                                   Column("UMI_threshold", [IsDtypeValidation(int)]),
-                                                   Column("follow_up_years", [IsDtypeValidation(int)]),
-                                                   Column("has_disease", [InListValidation(["y", "n"])]),
-                                                   Column("comment", [None])
-                ])
+DATA_TABLE_VALIDATE_AND_FORMAT_SCHEMA = Schema([Column("donor_id", null_validation),
+                                                Column("hasDonorAge", [IsDtypeValidation(int)]),
+                                                Column("has_phenotypic_sex", [InListValidation(has_phenotypic_sex_options)]),
+                                                Column("age_at_biopsy", [IsDtypeValidation(int)]),
+                                                Column("hasOrganismType", [InListValidation(has_organism_type_options)]),
+                                                # TODO: are there a list of sites, or is this a free form field
+                                                #    Column("hasAnatomicalSite", [None])
+                                                Column("neuropathology", [InListValidation(neuropathology_options)]),
+                                                #    Column("biosample_id", [None]),
+                                                Column("hasBioSampleType", [InListValidation(has_biosample_type_options)]),
+                                                Column("MMSE_Biopsy_value", [IsDtypeValidation(int)]),
+                                                Column("MMSE_Biopsy_atDonorAge", [IsDtypeValidation(int)]),
+                                                Column("MMSE_final_value", [IsDtypeValidation(int)]),
+                                                Column("MMSE_final_atDonorAge", [IsDtypeValidation(int)]),
+                                                Column("APOE_value", [IsDtypeValidation(int)]),
+                                                Column("APOE_atDonorAge", [IsDtypeValidation(int)]),
+                                                Column("DataModality", [InListValidation(data_modality_options)]),
+                                                # file path columns must start with "gs://"
+                                                # TODO: check starts with not just for pattern
+                                                Column("summary_file_path", [MatchesPatternValidation("gs://")]),
+                                                Column("features_file_path", [MatchesPatternValidation("gs://")]),
+                                                Column("matrix_file_path", [MatchesPatternValidation("gs://")]),
+                                                Column("barcode_file_path", [MatchesPatternValidation("gs://")]),
+                                                Column("biosample_id_of_file", null_validation),
+                                                Column("library_id", null_validation),
+                                                Column("UMI_threshold", [IsDtypeValidation(int)]),
+                                                Column("follow_up_years", [IsDtypeValidation(int)]),
+                                                Column("has_disease", [InListValidation(has_disease_options)])
+                                                   #    Column("comment", [None])
+                                            ])
 
 
 
