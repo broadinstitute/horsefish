d81db21788206e9c4e2f821a426ec74193100b7d
Sushma Chaluvadi <schaluva@broadinstitute.org>
2021-10-20 14:34:07 -0400

    clean up

--- scripts/emerge/one_step_data_ingest.py
+++ scripts/emerge/one_step_data_ingest.py
@@ -10,33 +10,37 @@ def create_recoded_json(row_json):
 
     for key in row_json.keys():  # for column name in row
         value = row_json[key]    # get value
-        # if value exists - not an empty cell/entry
-        if value is not None:
-            # and starts with 'gs://' --> recode path with expanded request
-            if value.startswith("gs://"):
-                relative_tdr_path = value.replace("gs://","/")  # create TDR relative path
-                # TODO: add in description = id_col + col_name
-                recoded_row_json[key] = {"sourcePath":value,
-                                "targetPath":relative_tdr_path,
-                                "mimeType":"text/plain"
-                                }
+        if value is not None:  # if value exists (non-empty cell)
+            if isinstance(value, str):  # and is a string
+                if value.startswith("gs://"):  # starting with gs://
+                    relative_tdr_path = value.replace("gs://","/")  # create TDR relative path
+                    # recode original value/path with expanded request
+                    # TODO: add in description = id_col + col_name
+                    recoded_row_json[key] = {"sourcePath":value,
+                                    "targetPath":relative_tdr_path,
+                                    "mimeType":"text/plain"
+                                    }
+                    continue
 
-            recoded_row_json_list = []  # instantiate empty list to store recoded values
-            # if value is of type array
-            if value.startswith("[") and value.endswith("]"):
-                value_list = json.loads(value)  # convert <str> to <liist>
-                # and list values start with 'gs://'
-                paths = [item.startswith('gs://') for item in list(value_list)]
-                if any(paths):
-                    # for each item in the array, create the json request
-                    for item in value_list:
-                        relative_tdr_path = item.replace("gs://","/")  # create TDR relative path
-                        x = {"sourcePath":item,
-                            "targetPath":relative_tdr_path,
-                            "mimeType":"text/plain"
-                            }
-                        recoded_row_json_list.append(x)  # add json request to list
-                recoded_row_json[key] = recoded_row_json_list  # add list of json requests to larger json request
+                recoded_row_json_list = []  # instantiate empty list to store recoded values for arrayOf:True cols
+                if value.startswith("[") and value.endswith("]"):  # if value is an array
+                    value_list = json.loads(value)  # convert <str> to <liist>
+                    paths = [item.startswith('gs://') for item in list(value_list)]  # and check if list values start with 'gs://'
+                    # TODO: any cases where an item in a list is not gs:// should be a user error?
+                    if any(paths):
+                        for item in value_list:  # for each item in the array
+                            relative_tdr_path = item.replace("gs://","/")  # create TDR relative path
+                            # create the json request for list member
+                            recoded_list_member = {"sourcePath":item,
+                                                   "targetPath":relative_tdr_path,
+                                                   "mimeType":"text/plain"
+                                                   }
+                            recoded_row_json_list.append(recoded_list_member)  # add json request to list
+                    recoded_row_json[key] = recoded_row_json_list  # add list of json requests to larger json request
+                    continue
+
+                # value is string but not a gs:// path or list of gs:// paths
+                recoded_row_json[key] = value
 
     return recoded_row_json
 
@@ -45,12 +49,13 @@ def create_newline_delimited_json(input_tsv):
     """Create newline delimited json file from input tsv."""
 
     tsv_df = pd.read_csv(input_tsv, sep="\t")
+    new_df = tsv_df.where(tsv_df.notnull(), None)  # has None where empty cells
 
     basename = input_tsv.split(".tsv")[0]
     output_filename = f"{basename}_recoded_newline_delimited.json"
 
     all_rows = []
-    for index, row in tsv_df.iterrows():
+    for index, row in new_df.iterrows():
         # recode the row json with gs:// path ingest format if applicable
         recoded_row_dict = create_recoded_json(row)
         # add recoded row's dictionary to list
