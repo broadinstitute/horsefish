8068e2d23f8ac232a428488ef0996e7d66dc40a4
Sushma Chaluvadi <schaluva@broadinstitute.org>
2022-02-22 15:47:48 -0500

    update script to handle arrays that are non gs path strings

--- scripts/emerge/WDL_write_arrays_wdl_outputs_to_TDR_ArraysOutputsTable.py
+++ scripts/emerge/WDL_write_arrays_wdl_outputs_to_TDR_ArraysOutputsTable.py
@@ -160,6 +160,11 @@ def create_recoded_json(row_json):
                         recoded_row_json[key] = recoded_row_json_list  # add list of json requests to larger json request
                         continue
 
+                    else:  # when list values are strings that DO NOT start with gs:// (like filerefs)
+                        for item in value_list:  # for each string item in non gs:// path array
+                            recoded_row_json_list.append(item)
+                        recoded_row_json[key] = recoded_row_json_list
+                        continue
                 # if value is string but not a gs:// path or list of gs:// paths
                 recoded_row_json[key] = value
 
