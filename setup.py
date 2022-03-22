46889b37d713a793430ed9162319ee9778d5fec0
Morgan Taylor <marymorg@broadinstitute.org>
2022-03-22 13:47:36 -0400

    wip setup

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup, find_packages
 
-setup(name='horsefish',
-      version='0.1',
-      packages = find_packages(),
-      )
+setup(
+      name='horsefish',
+      version='0.0.1',
+      description='Horsefish shared code',
+      packages=find_packages(where='./horsefish')
+)
