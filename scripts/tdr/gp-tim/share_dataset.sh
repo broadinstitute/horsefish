3400c01b6e438c1195570c85f8184a89291faeff
Morgan Taylor <marymorg@broadinstitute.org>
2022-05-06 16:36:36 -0400

    add relationships, fix dataset share script

--- scripts/tdr/gp-tim/share_dataset.sh
+++ scripts/tdr/gp-tim/share_dataset.sh
@@ -15,11 +15,11 @@ CUSTODIANS=$(cat $CONFIG_JSON | jq '.shareWith.custodian|.[]')
 for STEWARD_EMAIL in $STEWARDS
 do
     echo "assigning steward policy to $STEWARD_EMAIL on dataset $DATASET_ID"
-    curl -sL -X POST "https://data.terra.bio/api/repository/v1/datasets/${DATASET_ID}/policies/steward/members" -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -d "{ \"email\": \"${STEWARD_EMAIL}\"}" | jq
+    curl -sL -X POST "https://data.terra.bio/api/repository/v1/datasets/${DATASET_ID}/policies/steward/members" -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -d "{\"email\": ${STEWARD_EMAIL}}" | jq
 done
 
 for CUSTODIAN_EMAIL in $CUSTODIANS
 do
     echo "assigning custodian policy to $CUSTODIAN_EMAIL on dataset $DATASET_ID"
-    curl -sL -X POST "https://data.terra.bio/api/repository/v1/datasets/${DATASET_ID}/policies/custodian/members" -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -d "{ \"email\": \"${CUSTODIAN_EMAIL}\"}" | jq
+    curl -sL -X POST "https://data.terra.bio/api/repository/v1/datasets/${DATASET_ID}/policies/custodian/members" -H "accept: application/json" -H "Content-Type: application/json" -H "Authorization: Bearer ${TOKEN}" -d "{\"email\": ${CUSTODIAN_EMAIL}}" | jq
 done
