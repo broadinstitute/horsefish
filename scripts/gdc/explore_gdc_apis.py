745a60d2aa5bb34db30c2a23dee3f2977c4bdaf3
Sushma Chaluvadi <schaluva@broadinstitute.org>
2022-07-11 19:19:52 -0400

    add script to explore gdc apis

--- scripts/gdc/explore_gdc_apis.py
+++ scripts/gdc/explore_gdc_apis.py
@@ -6,6 +6,16 @@ import requests
 import json
 
 
+def read_auth_token(token_file):
+    """Read auth token from file."""
+
+    # read contents of auth token file into string
+    with open(token_file, "r") as token:
+        auth_token = str(token.read().strip())
+
+    return auth_token
+
+
 def get_gdc_file_metadata(sample_alias, token_file):
     """Get metadata about files from GDC based on sample name."""
 
@@ -14,8 +24,9 @@ def get_gdc_file_metadata(sample_alias, token_file):
     # TODO: what additional filters?
     filters = {"op":"=",
                "content":{
-                            "field": "file_name",
-                            "value": f"{sample_alias}*"
+                            "field": "submitter_id",
+                            "value": f"{sample_alias}"
+                            # "value": f"{sample_alias}.*"
                          }
               }
 
@@ -45,6 +56,23 @@ def get_gdc_file_metadata(sample_alias, token_file):
     print(f"Files endpoint response for {sample_alias} written to output file named {output_filename}.")
 
 
+def get_submission_status(auth_token):
+    """Call submissions endpoint to get status of submission."""
+
+    submissions_endpt = "https://api.gdc.cancer.gov/v0/submission/graphql"
+
+    query = {"query": "{file_name (project_id: \"TCGA-HNSC\") {\r\n    id\r\n    submitter_id\r\n    file_name\r\n    project_id\r\n}\r\n}"}
+
+    headers = {"X-Auth-Token": auth_token, "Content-type": "application/json"}
+    response = requests.post(submissions_endpt, headers=headers, json=query)
+
+    output_filename = f"submissions_response.tsv"
+    with open(output_filename, "w") as f:
+        f.write(response.text)
+
+    print(f"Check output file.")
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="GDC API to get file metadata.")
@@ -54,4 +82,8 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    get_gdc_file_metadata(args.sample_alias, args.token_file)
+    # TODO: python script to accept tsv of sample aliases? Or bash script to loop through file and run python script per sample.
+    auth_token = read_auth_token(args.token_file)
+    get_submission_status(auth_token)
+
+    # get_gdc_file_metadata(args.sample_alias, args.token_file)
