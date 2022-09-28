48519e6f17bff4d00ed0be60a8ba323446757bae
Morgan Taylor <marymorg@broadinstitute.org>
2022-09-28 12:11:52 -0400

    add supporting Dockerfile and documentation

---
+++ scripts/wfl/twisttcap/update_docker_image.sh
@@ -0,0 +1,9 @@
+set -e
+
+VERSION=$(cat setup_new_wfl_module.py | grep docker_version | cut -d'"' -f2)
+BUILD_TAG="broadinstitute/horsefish:twisttcap_wfl_setup_v${VERSION}"
+
+echo "using build tag: ${BUILD_TAG}"
+
+docker build . -t ${BUILD_TAG}
+docker push ${BUILD_TAG}
