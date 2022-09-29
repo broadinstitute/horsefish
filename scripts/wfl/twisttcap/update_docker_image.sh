0f89886176631c45125ed25f59413b11314d1019
Morgan Taylor <marymorg@broadinstitute.org>
2022-09-29 11:20:24 -0400

    one more module -> workload

--- scripts/wfl/twisttcap/update_docker_image.sh
+++ scripts/wfl/twisttcap/update_docker_image.sh
@@ -1,6 +1,6 @@
 set -e
 
-VERSION=$(cat setup_new_wfl_module.py | grep docker_version | cut -d'"' -f2)
+VERSION=$(cat setup_new_wfl_workload.py | grep docker_version | cut -d'"' -f2)
 BUILD_TAG="broadinstitute/horsefish:twisttcap_wfl_setup_v${VERSION}"
 
 echo "using build tag: ${BUILD_TAG}"
