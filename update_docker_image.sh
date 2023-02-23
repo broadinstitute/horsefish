edee045f1a12fc5e9b4c2fdc94b2a4f32a829521
bahill <bhill@broadinstitute.org>
2023-02-23 16:42:03 -0500

    adding requested prompt for overwrite vs new tag

--- update_docker_image.sh
+++ update_docker_image.sh
@@ -1,10 +1,19 @@
+#!/bin/bash
 set -e
 
 # DEVELOPER: update this field anytime you make a new docker image
-VERSION="2.0"
+VERSION="2.1"
 BUILD_TAG="broadinstitute/horsefish:v${VERSION}"
 
 echo "using build tag: ${BUILD_TAG}"
+
+if docker manifest inspect $BUILD_TAG > /dev/null ;
+then
+    echo "Docker image already exists. Do you want to overwrite this tag?."
+    read -p "Press [Enter] to overwrite or [Ctrl+C] to exit and change the version number."
+fi
+
+echo "Docker image does not exist. Building new image."
 
 docker build . -t ${BUILD_TAG}
 docker push ${BUILD_TAG}
