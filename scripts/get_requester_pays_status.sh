46cf4e3a27f2840aa64d0e29840f56e87ebfa497
samanthahv <68919432+samanthahv@users.noreply.github.com>
2023-05-02 16:02:36 -0400

    addressing PR comments

--- scripts/get_requester_pays_status.sh
+++ scripts/get_requester_pays_status.sh
@@ -1,13 +1,32 @@
 #!/bin/bash
 
+if (( $# < 2 )); then
+  echo "Usage: $0 TERRA_PROJECT_ID PATH_TO_TERRA_BUCKET_PATH"
+  echo "The Terra bucket path can be formatted as gs://fc-XXXXX or fc-XXXXX"
+  echo "i.e you can run ./set_requester_pays.sh on project_id fc-12345"
+  echo "NOTE: this script requires you to be authed as your firecloud.org admin account."
+  exit 0
+else
+  # grab bucket
+  if (( $# == 2 )); then
+    BUCKET=$2
+  elif (( $# > 2 )); then
+    # check if there are more than 2 arguments
+    echo "Usage: $0 TERRA_PROJECT_ID PATH_TO_TERRA_BUCKET_PATH"
+    echo "This script can only check the RP status of one bucket"
+    exit 0
+  fi
+fi
+
 PROJECT_ID=$1
-BUCKET=$2
 USER_EMAIL=$(gcloud config get-value account)
 MEMBER="user:${USER_EMAIL}"
 SLEEP_SEC=15
 
-
 ROLE="roles/storage.admin"
+# seems to be the only role that can grant storage.buckets.get permissions on the workspace bucket required to get the RP status
+# the end of the script revokes this role
+
 # enable requesterpays permissions
 echo "Enabling permissions for ${USER_EMAIL} to read bucket"
 gcloud beta projects add-iam-policy-binding $PROJECT_ID --member=$MEMBER --role=$ROLE | grep -A 1 -B 1 "${MEMBER}"
