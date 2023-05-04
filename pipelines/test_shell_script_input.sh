de53b8a3a1be462aa1d1867d64b1fa310de0312a
Sushma Chaluvadi <schaluva@broadinstitute.org>
2023-05-04 10:47:31 -0400

    test input sh script

---
+++ pipelines/test_shell_script_input.sh
@@ -0,0 +1,3 @@
+gsutil cp gs://schaluva-bucket/sample.json gs://schaluva-bucket/run_shell_script_tmp/sample.json
+gsutil cp gs://schaluva-bucket/1tb_test_source_details.csv gs://schaluva-bucket/run_shell_script_tmp/1tb_test_source_details.csv
+gsutil cp gs://schaluva-bucket/renamed_single_fastq.txt gs://schaluva-bucket/run_shell_script_tmp/renamed_single_fastq.txt
