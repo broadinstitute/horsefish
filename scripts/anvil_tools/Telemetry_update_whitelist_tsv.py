8b83d93613ff9b1f6abeaa471f91d3b6436a1db5
bahill <bhill@broadinstitute.org>
2023-07-05 15:30:17 -0400

    adding pseudo code for AT-346

---
+++ scripts/anvil_tools/Telemetry_update_whitelist_tsv.py
@@ -0,0 +1,15 @@
+# Best - no copy paste
+# What if you pulled the tab for the ncbiaccess config and committed the whole thing
+# issue is - how do you know what is new beyond the last line?
+# there is a PR ultimately & unit tests
+# Diff/validate
+
+# Better - requirs PMs to copy paste
+# func to convert ncbiaccess info to tsv format
+# return tsv - add that to whitelist.tsv
+
+
+# use snippet
+# git clone
+# cd/ to the directory where whitelist.tsv is located
+# take formatted input
