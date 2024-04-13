6d3c06e6a36fe58ab269205e67dd22100a636c42
Sam Novod <samn@broadinstitute.org>
2024-04-13 14:22:54 -0400

    get job status can take bge or wgs

--- scripts/dragen_reprocessing/get_batch_job_status.py
+++ scripts/dragen_reprocessing/get_batch_job_status.py
@@ -3,7 +3,6 @@ import google.auth
 import logging
 import re
 from typing import Optional, Any
-from dateutil.relativedelta import relativedelta
 from google.cloud import bigquery
 
 
@@ -13,17 +12,18 @@ logging.basicConfig(
 
 
 class GetSampleInfo:
-    def __init__(self, google_project: str, minimum_run_date: str, maximum_run_date: str):
+    def __init__(self, google_project: str, minimum_run_date: str, maximum_run_date: str, data_type: str):
         credentials, your_project_id = google.auth.default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
         self.google_project = google_project
         self.bqclient = bigquery.Client(credentials=credentials, project=google_project)
         self.minimum_run_date = minimum_run_date
         self.maximum_run_date = maximum_run_date
+        self.data_type = data_type
 
     def _query_for_batch_job_info(self) -> Any:
         """Gets all batch job info from bigquery"""
-        query_string = f"""SELECT a.job_id, a.input_path, s.status, s.task_id, a.timestamp as submit_time, a.output_path,
-  s.timestamp as task_time
+        query_string = f"""SELECT a.job_id, a.input_path, s.status, s.task_id, a.timestamp as submit_time, 
+        a.output_path, s.timestamp as task_time, a.sample_id
 FROM `{self.google_project}.dragen_illumina.job_array` as a
 join `{self.google_project}.dragen_illumina.tasks_status` as s on a.job_id = s.job_id
   and CAST(a.batch_task_index AS STRING)=REGEXP_EXTRACT(task_id, r'group0-(\d+)')
@@ -64,12 +64,14 @@ and DATETIME "{self.minimum_run_date}" < a.timestamp"""
         # assume path like gs://{bucket}/{sample set}/{project}/{data_type}/{sample}/v{version}/{sample}.cram
         match = re.search(r'gs://\S+/\S+/(\S+)/(\S+)/(\S+)/v(\d+)/\S+.cram', cram_path)
         if match:
+            project = match.group(1)
+            data_type = match.group(2)
+            sample = match.group(3)
+            version = match.group(4)
+            terra_sample_id = f"{project}_{sample}_v{version}_{data_type}_GCP".replace(".", "_")
             return {
-                'project': match.group(1),
-                'data_type': match.group(2),
-                'sample': match.group(3),
-                'version': match.group(4),
-                'sample_id': f'{match.group(1)}.{match.group(2)}.{match.group(3)}.{match.group(4)}'
+                'terra_sample_id': terra_sample_id,
+                'sample_id': f'{project}.{data_type}.{sample}.{version}'
             }
         return None
 
@@ -77,10 +79,17 @@ and DATETIME "{self.minimum_run_date}" < a.timestamp"""
         """Creates full dictionary where key is sample name and value is dict with all jobs info"""
         samples_dict = {}
         for row in query_results:
-            # Get sample information from cram input
-            sample_information = self._get_sample_information_from_cram_path(
-                row['input_path'].replace('s3://', 'gs://')
-            )
+            if self.data_type == 'bge':
+                # Get sample information from cram input
+                sample_information = self._get_sample_information_from_cram_path(
+                    row['input_path'].replace('s3://', 'gs://')
+                )
+            else:
+                # wgs data_type sample_id is collab_id
+                sample_information = {
+                    'sample_id': row['sample_id'],
+                    'terra_sample_id': row['sample_id']
+                }
             if sample_information:
                 sample_id = sample_information['sample_id']
                 if sample_id not in samples_dict:
@@ -125,17 +134,13 @@ class CreateSampleTsv:
         self.samples_dict = samples_dict
         self.output_tsv = output_tsv
 
-    @staticmethod
-    def _create_terra_sample_id(sample_dict):
-        return f"{sample_dict['project']}_{sample_dict['sample']}_v{sample_dict['version']}_{sample_dict['data_type']}_GCP".replace(".", "_")
-
     def create_tsv(self):
         logging.info(f"Creating {self.output_tsv}")
         with open(output_tsv, 'w') as f:
             f.write('entity:sample_id\tattempts\tlatest_status\tlatest_task_id\toutput_path\tlast_attempt\trunning_time\n')
             for sample_dict in samples_dict.values():
                 f.write(
-                    f"{self._create_terra_sample_id(sample_dict)}\t{len(sample_dict['job_ids'])}\t" +
+                    f"{sample_dict['terra_sample_id']}\t{len(sample_dict['job_ids'])}\t" +
                     f"{sample_dict['latest_status']}\t{sample_dict['latest_task_id']}\t" +
                     f"{sample_dict['output_path']}\t{sample_dict['latest_timestamp']}\t" +
                     f"{str(sample_dict['running_time_dict'][sample_dict['latest_job_id']])}\n"
@@ -147,6 +152,9 @@ def get_args() -> Namespace:
 
     parser.add_argument('-g', '--gcp_project', type=str, help='Google project used for BigQuery.',
                         choices=['gp-cloud-dragen-dev', 'gp-cloud-dragen-prod', 'dsp-cloud-dragen-stanley'], required=True)
+    parser.add_argument('-d', '--data_type', type=str, choices=['wgs', 'bge'],
+                        help='bge assumes data was delivered by ops and sample_id concats multiple columns. wgs assumes sample id = collab_id',
+                        required=True)
     parser.add_argument('-t', '--output_tsv', type=str, help='path for output sample tsv', required=True)
     parser.add_argument('-a', '--min_start_date', type=str, help='tasks created after this time. YYYY-MM-DD', default="2008-10-30")
     parser.add_argument('-b', '--max_start_date', type=str, help='tasks created before this time. YYYY-MM-DD', default="2028-10-30")
@@ -155,8 +163,11 @@ def get_args() -> Namespace:
 
 if __name__ == "__main__":
     args = get_args()
-    gcp_project, output_tsv, min_start_date, max_start_date = args.gcp_project, args.output_tsv, args.min_start_date, args.max_start_date
+    gcp_project, output_tsv, min_start_date, max_start_date, data_type = args.gcp_project, args.output_tsv, args.min_start_date, args.max_start_date, args.data_type
 
-    samples_dict = GetSampleInfo(google_project=gcp_project, maximum_run_date=max_start_date, minimum_run_date=min_start_date).run()
+    samples_dict = GetSampleInfo(google_project=gcp_project, maximum_run_date=max_start_date, minimum_run_date=min_start_date, data_type=data_type).run()
     CreateSampleTsv(samples_dict=samples_dict, output_tsv=output_tsv).create_tsv()
-    print(f"To upload tsv to workspace run:\npython3 scripts/import_large_tsv/import_large_tsv.py --project <workspace-project> --workspace <workspace_name> --tsv {output_tsv}")
+    print(
+        f"To upload tsv to workspace run the below in terra-tools repo or upload tsv manually:\n" 
+        "python3 scripts/import_large_tsv/import_large_tsv.py --project <workspace-project> --workspace <workspace_name> --tsv {output_tsv}"
+    )
