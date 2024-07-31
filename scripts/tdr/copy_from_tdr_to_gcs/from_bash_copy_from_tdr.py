48ddc849a26221a595eda1f302c7651a2e81276e
bahill <bhill@broadinstitute.org>
2024-07-31 17:52:34 -0400

    continuing to convert pseudo code to functional code

--- scripts/tdr/copy_from_tdr_to_gcs/from_bash_copy_from_tdr.py
+++ scripts/tdr/copy_from_tdr_to_gcs/from_bash_copy_from_tdr.py
@@ -1,42 +1,71 @@
 import os
 import sys
 import csv
+import logging
 import json
+import re
 import requests
+import google.auth
+import google.auth.transport.requests
 import subprocess
-from google.auth import compute_engine
-from google.auth.transport.requests import Request
 
 STAGING_AREA_BUCKETS = {
-    "prod": {
         "EBI": "gs://broad-dsp-monster-hca-prod-ebi-storage/prod",
         "UCSC": "gs://broad-dsp-monster-hca-prod-ebi-storage/prod",
         "LANTERN": "gs://broad-dsp-monster-hca-prod-lantern",
         "LATTICE": "gs://broad-dsp-monster-hca-prod-lattice/staging",
         "TEST": "gs://broad-dsp-monster-hca-prod-ebi-storage/broad_test_dataset"
-    }
 }
 
-# input should be a manifest csv of those projects that need data copied back
-# Check if n csv_path is provided as an argument
-if len(sys.argv) != 2:
-    print("Usage: python3 copy_from_tdr.py <csv_path>")
-    sys.exit(1)
+def setup_cli_logging_format() -> None:
+    logging.basicConfig(level=logging.INFO, format='%(message)s', stream=sys.stdout)
 
-csv_path = sys.argv[1]
+def validate_input(csv_path:str):
+    """
+    input should be a manifest csv of those projects that need data copied back
+    format is <institution>,<project_id>
+    """
+    if not os.path.isfile(csv_path):
+        logging.debug(f"{csv_path} not found")
+        sys.exit(1)
 
-# Check if the file exists
-if not os.path.isfile(csv_path):
-    print(f"{csv_path} not found")
-    sys.exit(1)
+    if not csv_path.endswith('.csv'):
+        logging.debug(f"{csv_path} is not a csv file")
+        sys.exit(1)
 
-# Check if the file is a csv
-if not csv_path.endswith('.csv'):
-    print(f"{csv_path} is not a csv file")
-    sys.exit(1)
+    else:
+        return csv_path
+
+def find_project_id_in_str(s: str) -> str:
+    """
+    The selected function find_project_id_in_str(s: str) -> str:
+    is used to extract a UUID (Universally Unique Identifier) from a given string s.
+    :param s:
+    :return:
+    Attribution:
+    https://github.com/DataBiosphere/hca-ingest/blob/main/orchestration/hca_orchestration/support/matchers.py
+    """
+    uuid_matcher = re.compile('[a-f0-9]{8}-?[a-f0-9]{4}-?4[a-f0-9]{3}-?[89ab][a-f0-9]{3}-?[a-f0-9]{12}', re.I)
+    project_ids = uuid_matcher.findall(s)
+
+    if len(project_ids) != 1:
+        raise Exception(f"Found more than one or zero project UUIDs in {s}")
+
+    return str(project_ids[0])
+
+def _sanitize_gs_path(path: str) -> str:
+    return path.strip().strip("/")
 
 def _parse_csv(csv_path:str):
-    keys = set()
+    """
+    Parses the csv file and returns a list of staging areas
+    :param csv_path:
+    :return:
+    Attribution:
+    https://github.com/DataBiosphere/hca-ingest/blob/main/orchestration/hca_manage/manifest.py
+    """
+    gs_paths = set()
+    project_ids = set()
     with open(csv_path, "r") as f:
         reader = csv.reader(f)
         for row in reader:
@@ -48,61 +77,60 @@ def _parse_csv(csv_path:str):
             institution = row[0]
             project_id = find_project_id_in_str(row[1])
 
-            key = None
-            if project_id_only:
-                project_id = row[1]
-                key = project_id
-            else:
+            project_ids.add(project_id)
 
-                if institution not in STAGING_AREA_BUCKETS[env]:
-                    raise Exception(f"Unknown institution {institution} found")
+            gs_path = None
+            if institution not in STAGING_AREA_BUCKETS:
+                raise Exception(f"Unknown institution {institution} found")
 
-                institution_bucket = STAGING_AREA_BUCKETS[env][institution]
-                path = institution_bucket + "/" + project_id
+            institution_bucket = STAGING_AREA_BUCKETS[institution]
+            path = institution_bucket + "/" + project_id
 
-                # sanitize and dedupe
-                path = _sanitize_gs_path(path)
-                assert path.startswith("gs://"), "Staging area path must start with gs:// scheme"
-                key = path
+            # sanitize and dedupe
+            path = _sanitize_gs_path(path)
+            assert path.startswith("gs://"), "Staging area path must start with gs:// scheme"
+            gs_path = path
 
-            if include_release_tag:
-                key = key + f",{release_tag}"
-            keys.add(key)
+            gs_paths.add(gs_path)
 
-    chunked_paths = chunked(keys, MAX_STAGING_AREAS_PER_PARTITION_SET)
-    return [chunk for chunk in chunked_paths]
+        # print(f"These are the parsed gs_paths {gs_paths}")
+        # print(f"These are the parsed project_ids {project_ids}")
+        return gs_paths, project_ids
 
-# Read the file line by line
-with open csv_path, 'r') as file:
-    lines = file.read().splitlines()
-    #
 
-print(lines)
+def _get_target_snapshot_ids(project_ids: set[str]) -> set[str]:
+    """
+    This function gets the target snapshot name filters for the given project ids
+    :param project_ids:
+    :return:
+    """
+    target_snapshots = set()
+    for project in project_ids:
+        target_snapshot = f"hca_prod_{project.replace('-', '')}"
+        target_snapshots.add(target_snapshot)
+    return target_snapshots
 
-# TODO create the list of snapshot IDs from the list of UUIDs
-# use manifest.csv to get the UUIDs that need data copied back
-# for each UUID, construct the snapshot name - which will be latest snapshot with a dataset id like
-# "hca_prod_<uuid-without-dashes>*"
-# use those snapshot ids to get the latest snapshot id for each dataset
-# this then becomes the snapshot list
 
-snapshots = []
-for line in lines:
+# TODO: make this work in a Docker image
+def get_access_token():
+    creds, project = google.auth.default()
+    auth_req = google.auth.transport.requests.Request()
+    creds.refresh(auth_req)
+    access_token = creds.token
+    return access_token
 
-# TODO - is this needed? or can we just run locally as Monster members?
-# Get access token
-# credentials = compute_engine.Credentials()
-# credentials.refresh(Request())
-# access_token = credentials.token
 
-# for snapshot in lines:
-#     # Make request to the API with the current snapshot
-#     response = requests.get(f"https://data.terra.bio/api/repository/v1/snapshots/{snapshot}/files?offset=0&limit=10000",
-#                             headers={'accept': 'application/json', 'Authorization': f'Bearer {access_token}'})
-#
-#     # Write the response to a JSON file
-#     with open(f"response_{snapshot}.json", 'w') as outfile:
-#         json.dump(response.json(), outfile)
+def _get_latest_snapshot(target_snapshots: set[str], access_token: str):
+    for snapshot_name in target_snapshots:
+        response = requests.get(f'https://data.terra.bio/api/repository/v1/snapshots?sort=createdDate,'
+                                f'desc&limit=1&filter={snapshot_name}',
+                                headers={'accept': 'application/json', 'Authorization': f'Bearer {access_token}'})
+        response.raise_for_status()
+        with open(f"response_{snapshot_name}.json", 'w') as outfile:
+            json.dump(response.json(), outfile)
+
+# TODO 8/1/24 - got a 400 for invalid filter - yay!
+# need to add note that you need to gcloud auth to run this.
 #
 #     # Extract file details from the JSON file and append them to a text file
 #     with open(f"response_{snapshot}.json", 'r') as json_file:
@@ -126,3 +154,27 @@ for line in lines:
 # #  "LATTICE": "gs://broad-dsp-monster-hca-prod-lattice/staging",
 # for access_url in access_urls:
 #     subprocess.run(['gcloud storage', 'cp', access_url, "<INSERT STAGING /DATA url>"])
+
+
+def main():
+    """Parse command-line arguments and run specified tool.
+
+     Note: Does not take explicit input arguments, but uses sys.argv inputs
+     from the command line.
+
+    """
+    setup_cli_logging_format()
+    access_token = get_access_token()
+    csv_path = sys.argv[1]
+    validate_input(csv_path)
+    gs_paths = _parse_csv(csv_path)[0]
+    print(f"gs_paths are {gs_paths}")
+    project_ids = _parse_csv(csv_path)[1]
+    print(f"project_ids are {project_ids}")
+    target_snapshots = _get_target_snapshot_ids(project_ids)
+    print(f"target snapshot ids are {target_snapshots}")
+    tdr_data_path = _get_latest_snapshot(target_snapshots, access_token)
+
+
+if __name__ == '__main__':
+    sys.exit(main())
