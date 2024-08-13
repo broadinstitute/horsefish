cfc2cded38e60eb5a082f273f839580b28effb4f
bahill <bhill@broadinstitute.org>
2024-08-13 16:32:57 -0400

    A little more code re-org an optimization to be sure that we never mix up lists

--- scripts/tdr/copy_from_tdr_to_gcs_hca/copy_from_tdr_to_gcs_hca.py
+++ scripts/tdr/copy_from_tdr_to_gcs_hca/copy_from_tdr_to_gcs_hca.py
@@ -1,3 +1,6 @@
+# Description: This script copies data from Terra Data Repository (TDR) HCA project buckets to HCA staging area buckets.
+# It is based on the bash script get_snapshot_files_and_transfer.sh, written by Samantha Velasquez.
+
 import os
 import sys
 import csv
@@ -15,8 +18,6 @@ STAGING_AREA_BUCKETS = {
         "LATTICE": "gs://broad-dsp-monster-hca-prod-lattice/staging",
         "TEST": "gs://broad-dsp-monster-hca-prod-ebi-storage/broad_test_dataset"
 }
-
-# TODO change prints to logging
 
 
 def setup_cli_logging_format() -> None:
@@ -70,8 +71,7 @@ def _parse_csv(csv_path:str):
     Attribution:
     https://github.com/DataBiosphere/hca-ingest/blob/main/orchestration/hca_manage/manifest.py
     """
-    staging_gs_paths = set()
-    project_ids = set()
+    tuple_list = []
     with open(csv_path, "r") as f:
         reader = csv.reader(f)
         for row in reader:
@@ -82,8 +82,6 @@ def _parse_csv(csv_path:str):
             assert len(row) == 2
             institution = row[0]
             project_id = find_project_id_in_str(row[1])
-
-            project_ids.add(project_id)
 
             if institution not in STAGING_AREA_BUCKETS:
                 raise Exception(f"Unknown institution {institution} found. "
@@ -97,25 +95,11 @@ def _parse_csv(csv_path:str):
             assert path.startswith("gs://"), "Staging area path must start with gs:// scheme"
             staging_gs_path = path
 
-            staging_gs_paths.add(staging_gs_path)
+            tuple_list.append((staging_gs_path, project_id))
 
-        return staging_gs_paths, project_ids
-
-
-def _get_target_snapshot_ids(project_ids: set[str]) -> set[str]:
-    """
-    This function gets the target snapshot name filters for the given project ids
-    :param project_ids:
-    :return:
-    """
-    target_snapshots = set()
-    for project in project_ids:
-        target_snapshot = f"hca_prod_{project.replace('-', '')}"
-        target_snapshots.add(target_snapshot)
-    return target_snapshots
+        return tuple_list
 
 
-# TODO: make this work in a Docker image
 def get_access_token():
     creds, project = google.auth.default()
     auth_req = google.auth.transport.requests.Request()
@@ -124,40 +108,40 @@ def get_access_token():
     return access_token
 
 
-def _get_latest_snapshots(target_snapshots: set[str], access_token: str):
-    latest_snapshots = []
-    for snapshot_name in target_snapshots:
-        snapshot_response = requests.get(
-            f'https://data.terra.bio/api/repository/v1/snapshots?offset=0&limit=10&sort=created_date&direction=desc&filter={snapshot_name}',
-            headers={'accept': 'application/json', 'Authorization': f'Bearer {access_token}'}
-        )
-        snapshot_response.raise_for_status()
-        latest_snapshot_id = snapshot_response.json()['items'][0]['id']
-        latest_snapshots.append(latest_snapshot_id)
-    return latest_snapshots
+def get_latest_snapshot(target_snapshot: str, access_token: str):
+    snapshot_response = requests.get(
+        f'https://data.terra.bio/api/repository/v1/snapshots?offset=0&limit=10&sort=created_date&direction=desc&filter='
+        f'{target_snapshot}',
+        headers={'accept': 'application/json', 'Authorization': f'Bearer {access_token}'}
+    )
+    snapshot_response.raise_for_status()
+    latest_snapshot_id = snapshot_response.json()['items'][0]['id']
+    return latest_snapshot_id
 
 
 # for each snapshot get access url and add to a list of access urls for that snapshot
-def get_access_urls(latest_snapshot_ids: list[str], access_token: str):
-    for snapshot in latest_snapshot_ids:
-        files_response = requests.get(
-            f'https://data.terra.bio/api/repository/v1/snapshots/{snapshot}/files?offset=0',
-            headers={'accept': 'application/json', 'Authorization': f'Bearer {access_token}'}
-        )
-        files_response.raise_for_status()
+def get_access_urls(snapshot: str, access_token: str):
+    list_of_access_urls = []
+    # for snapshot in latest_snapshot_ids:
+    logging.info(f"getting access urls for snapshot {snapshot}")
+    files_response = requests.get(
+        f'https://data.terra.bio/api/repository/v1/snapshots/{snapshot}/files?offset=0',
+        headers={'accept': 'application/json', 'Authorization': f'Bearer {access_token}'}
+    )
+    files_response.raise_for_status()
 
-        # Extract file details from the JSON file and add them to a list
-        list_of_access_urls = []
-        data = files_response.json()
-        for item in data:
-            list_of_access_urls.append(item['fileDetail']['accessUrl'])
-        return list_of_access_urls
+    # Extract file details from the JSON file and add them to a list
+    data = files_response.json()
+    for item in data:
+        list_of_access_urls.append(item['fileDetail']['accessUrl'])
+    return list_of_access_urls
 
 
-def copy_tdr_to_staging(access_urls: list[str], staging_gs_paths: set[str]):
+def check_staging_is_empty(staging_gs_paths: set[str]):
+    nonempty_staging_areas = []
     for staging_dir in staging_gs_paths:
         staging_data_dir = staging_dir + '/data/'
-        logging.info(f'staging_data_dir is {staging_data_dir}')
+        logging.info(f'checking contents of staging_data dir: {staging_data_dir}')
         # using gsutil as output is cleaner & faster
         output = subprocess.run(['gsutil', 'ls', staging_data_dir], capture_output=True)
         stdout = output.stdout.strip()
@@ -165,17 +149,42 @@ def copy_tdr_to_staging(access_urls: list[str], staging_gs_paths: set[str]):
         if len(files) > 1:
             logging.error(f"Staging area {staging_data_dir} is not empty")
             logging.info(f"files in staging area are: {files}")
-            continue
-    else:
-        logging.info(f"Staging area {staging_data_dir} is empty - copying files now")
+            nonempty_staging_areas.append(staging_data_dir)
+        else:
+            logging.info(f"Staging area {staging_data_dir} is empty")
+
+    if len(nonempty_staging_areas) > 0:
+        logging.error("One or more staging areas are not empty. Exiting.")
+        logging.info(f"Non-empty staging areas are: {nonempty_staging_areas}")
+        sys.exit(1)
+
+
+def copy_tdr_to_staging(tuple_list: list[tuple[str, str]], access_token: str):
+    for project_id in set([x[1] for x in tuple_list]):
+        target_snapshot = f"hca_prod_{project_id.replace('-', '')}"
+        latest_snapshot_id = get_latest_snapshot(target_snapshot, access_token)
+        logging.info(f'latest snapshot id for project {project_id} is {latest_snapshot_id}')
+        access_urls = get_access_urls(latest_snapshot_id, access_token)
+        num_access_urls = len(access_urls)
+        staging_gs_path = [x[0] for x in tuple_list if x[1] == project_id][0]
+        staging_data_dir = staging_gs_path + '/data/'
+        logging.info(f'Copying {num_access_urls} files from snapshot {latest_snapshot_id} to staging area {staging_data_dir}')
         for access_url in access_urls:
+            # strip the filename from the access url because gcp is not a file system - it's all objects
+            filename = access_url.split('/')[-1]
+            logging.info(f'access_url for snapshot {latest_snapshot_id} is {access_url}')
             try:
-                # strip the filename from the access url because gcp is not a file system - it's all objects
-                filename = access_url.split('/')[-1]
-                print(f"Copying {access_url} to {staging_data_dir}{filename}")
                 subprocess.run(['gcloud', 'storage', 'cp', access_url, staging_data_dir + filename])
             except Exception as e:
-                logging.error(f"Error copying {access_url} to {staging_data_dir}{filename}: {e}")
+                logging.error(f'Error copying {access_url} to {staging_gs_path}{filename}: {e}')
+                continue
+        # visual summary of number of files copied
+        files_copied = subprocess.run(['gsutil', 'ls', staging_data_dir],
+                                      capture_output=True).stdout.decode('utf-8').split('\n')
+        # gsutil outputs the dir and a blank line, so we need to remove the blank line and the dir to count files
+        files_in_dir = [x.split('/')[-1] for x in files_copied if x and x.split('/')[-1]]
+        number_files_copied = len(files_in_dir)
+        logging.info(f'{number_files_copied} out of {num_access_urls} files copied to {staging_data_dir}')
 
 
 def main():
@@ -188,28 +197,19 @@ def main():
     setup_cli_logging_format()
     access_token = get_access_token()
 
-    # read in the manifest and parse out the staging gs paths and project ids
+    # read in the manifest and get a tuple list of staging gs paths and project ids
     csv_path = sys.argv[1]
     validate_input(csv_path)
-    staging_gs_paths = _parse_csv(csv_path)[0]
-    logging.info(f"staging_gs_paths are {staging_gs_paths}")
-    project_ids = _parse_csv(csv_path)[1]
-    logging.info(f"project_ids are {project_ids}")
+    tuple_list = _parse_csv(csv_path)
+    logging.info(f"staging_gs_paths and project id tuple list is {tuple_list}")
 
-    # get the target snapshot ids, based on standard HCA ingest naming conventions
-    target_snapshots = _get_target_snapshot_ids(project_ids)
-    logging.info(f"target snapshot ids are {target_snapshots}")
+    # staging dir is the first element in each tuple
+    staging_gs_paths = set([x[0] for x in tuple_list])
 
-    # get the latest snapshot ids for each target snapshot
-    latest_snapshot_ids = _get_latest_snapshots(target_snapshots, access_token)
-    logging.info(f"latest_snapshots_ids are {latest_snapshot_ids}")
-
-    # get the access urls for each file in the snapshot
-    access_urls = get_access_urls(latest_snapshot_ids, access_token)
-    print(f"access_urls are {access_urls}")
-
+    # check if the staging area is empty
+    check_staging_is_empty(staging_gs_paths)
     # copy the files from the TDR project bucket to the staging area bucket
-    copy_tdr_to_staging(access_urls, staging_gs_paths)
+    copy_tdr_to_staging(tuple_list, access_token)
 
 
 if __name__ == '__main__':
